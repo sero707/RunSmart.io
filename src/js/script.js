1593590d18f866fa5ef0eaf8aac9b1248276aded
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2022-08-16 21:12:07 +0300

    add slider section adaptive (build)

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,17 @@ $(document).ready(function(){
     $('.slider__slider').slick({
         speed: 1200,
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 3000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/array.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/icon/array.png"></button>',
+        responsive: [
+            {
+              breakpoint: 950,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+        ]
     });
   });
