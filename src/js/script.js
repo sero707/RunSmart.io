f946320c1fdf4a523813f30633d6a177259880f8
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2022-08-26 17:31:02 +0300

    update catalog section (add tabs)

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,12 @@ $(document).ready(function(){
         ]
     });
 
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
     function toogleSlide(item){
       $(item).each(function(i) {
         $(this).on('click', function(e){
